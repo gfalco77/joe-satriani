812a28a9174e508d94129d2dff13eec7d641b801
gfalco77 <giuseppefalco77@gmail.com>
2024-03-20 17:14:32 +0000

    first commit

---
+++ src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,86 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useState} from 'react'
+import './AddProduct.css'
+import {useForm} from "react-hook-form";
+import upload_area from '../../assets/upload_area.svg'
+import axios from 'axios';
+
+const AddProduct = () => {
+  const [successMsg, setSuccessMsg] = useState("");
+  const [serverErrorMsg, setServerErrorMsg] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+    reset
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    await axios.post('https://1m61y8i2dk.execute-api.eu-west-2.amazonaws.com/Stage/products', data)
+    .then(response => {
+      setSuccessMsg("Product has been saved.");
+      reset();
+    })
+    .catch(error => {
+      setServerErrorMsg(error.message);
+    });
+  };
+
+  const [image, setImage] = useState(false);
+
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  return (
+      <div className='add-product'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {successMsg && <p className="success-message">{successMsg}</p>}
+          <div className='addproduct-itemfield'>
+            <p>Product Sku</p>
+            <input type="text" {...register("sku", {required: "Product sku is required."})}/>
+            {errors.sku && (<p className="error-message">{errors.sku.message}</p>)}
+          </div>
+          <div className='addproduct-itemfield'>
+            <p>Product Name</p>
+            <input type="text" {...register("name", {required: "Product Name is required."})}/>
+            {errors.name && (<p className="error-message">{errors.name.message}</p>)}
+          </div>
+          <div className='addproduct-itemfield'>
+            <p>Product Description</p>
+            <input type="text" {...register("description", {required: "Product Description is required."})}/>
+            {errors.description && (<p className="error-message">{errors.description.message}</p>)}
+          </div>
+          <div className='addproduct-price'>
+            <div className='addproduct-itemfield'>
+              <p>Price</p>
+              <input type="text" {...register("price", {required: "Product Price is required."})}/>
+              {errors.price && (<p className="error-message">{errors.price.message}</p>)}
+            </div>
+            <div className='addproduct-itemfield'>
+              <p>Now Price</p>
+              <input type="text" {...register("now_price")}/>
+            </div>
+          </div>
+          <div className='addproduct-itemfield'>
+            <p>Product Category</p>
+            <select name='category' className='add-product-selector' {...register("category")}>
+              <option value="women">Women</option>
+              <option value="men">Men</option>
+              <option value="kid">Kids</option>
+            </select>
+          </div>
+          <div className='addproduct-itemfield'>
+            <label htmlFor="file-input">
+              <img src={image ? URL.createObjectURL(image) : upload_area} alt="" className='addproduct-img'/>
+            </label>
+            <input onChange={imageHandler} type="file" name='image' id='file-input' hidden/>
+          </div>
+          <button className='addproduct-btn' type="submit">ADD</button>
+          {serverErrorMsg && <p className="error-message">{serverErrorMsg}</p>}
+        </form>
+      </div>
+  )
+}
+export default AddProduct
---
+++ src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect} from 'react'
+import './ListProduct.css'
+import axios from 'axios';
+import remove_icon from '../../assets/cross_icon.png'
+
+const ListProduct = () => {
+
+  const [products, setProducts] = React.useState([]);
+
+  const fetchProducts = async () => {
+    await axios.get('https://1m61y8i2dk.execute-api.eu-west-2.amazonaws.com/Stage/products')
+    .then(response => response.data)
+    .then((data) => {
+      setProducts(data)
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    return () => {
+      fetchProducts();
+    };
+  }, []);
+
+  const removeProduct = async (id) => {
+    await axios.delete('https://1m61y8i2dk.execute-api.eu-west-2.amazonaws.com/Stage/products/', {id: id})
+    .then(() => {
+      fetchProducts();
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error);
+    });
+  }
+
+  return (
+      <div className='list-product'>
+        <h1>Products</h1>
+        <div className='listproduct-format-main'>
+          <p>Products</p>
+          <p>Title</p>
+          <p>Was Price</p>
+          <p>Now Price</p>
+          <p>Category</p>
+          <p>Remove</p>
+        </div>
+        <div className='listproduct-allproducts'>
+          <hr/>
+          {products.map((product) => {
+            return (
+                <div className='listproduct-format-main listproduct-format' key={product.PK}>
+                  <img src={product.image} alt="" className='listproduct-product-img'/>
+                  <p>{product.name}</p>
+                  <p>£{product.was_price}</p>
+                  <p>£{product.now_price}</p>
+                  <p>£{product.category}</p>
+                  <img onClick={() => {
+                    removeProduct(product.PK)
+                  }} src={remove_icon} alt="" className='listproduct-remove-icon'/>
+                </div>
+            )
+          })}
+        </div>
+      </div>
+  )
+}
+export default ListProduct
